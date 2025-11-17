e3af93b057f0bc08bfd53822eda8bf3de148c463
SnehaBansal1605 <82718956+SnehaBansal1605@users.noreply.github.com>
2025-11-17 11:38:23 -0500

    Update Header.jsx

--- multiiq-consortium/src/components/Header.jsx
+++ multiiq-consortium/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { Button } from "../components/ui/button";
-import { NavLink } from "../components/NavLink";
+//import { NavLink } from "../components/NavLink";
 import logoNavy from "../assets/Logo-Dark.PNG";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
